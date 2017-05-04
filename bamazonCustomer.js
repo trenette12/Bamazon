15f5f075fe7d7886859e5c085a2cd0fd5c7ffba2
Tiffany Hyatt <Hyatt@Tiffanys-iMac.home>
2017-05-03 21:20:42 -0400

    made final update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -36,27 +36,30 @@ var customerOrder = function(){
       message: 'How many units would you like to purchase?'
     },
   ]).then(function(answers){
+    //setting variables for user input value to customerQuantity and itemId so we can check against them in later function
     var customerQuantity = parseInt(answers.quantity);
     var itemId = parseInt(answers.itemid);
       connection.query('SELECT * FROM products WHERE ?', {item_id: itemId},
         function(error,response){
           for (var i = 0; i < response.length; i++){
             var stockQuantity = response[i].stock_quantity;
+            var customerPrice = response[i].price;
+            var totalPrice = customerPrice * customerQuantity;
               if(customerQuantity < stockQuantity){
               console.log("You're lucky, we have some in stock!");
                 connection.query('UPDATE products SET ? WHERE ?', [{stock_quantity: stockQuantity - customerQuantity}, {item_id: itemId}],
                   function(error,response){
                     console.log('response', response);
                     for (var i = 0; i < response.length; i++){
-                      var customerPrice = response[i].price;
-                    console.log('Your order has been placed and your total is $' + (customerPrice * customerQuantity));
-                  }
-                });
-              } else {
+                    console.log('Your order has been placed and your total is $' + totalPrice);
+                  }//closes for loop
+                })//closes mySQL query
+              }//closes if Statement for comparing customerQuantity and stockQuantity
+             else {
               console.log("Insuffient Quantity!");
               }
-        }
-    });
-  });
-}
+            }//closes first for loop
+        });//closes initial mySQL query
+    });//closes then function
+  };//closes customerOrder function
 customerOrder();
